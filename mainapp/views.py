963f52dbf678efb2d3dc125d5a577f4b914e278a
Ilya Kochagin <ilyakochagin@yandex.ru>
2022-05-07 01:08:34 +0300

    initial commit

---
+++ mainapp/views.py
@@ -0,0 +1,50 @@
+# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("homepage")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request=request, template_name="register.html", context={"register_form": form})
+
+
+def login_req(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("homepage")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="login.html", context={"login_form": form})
+
+
+@login_required(login_url='/login')
+def homepage(request):
+    return render(request=request, template_name="homepage.html", context={})
+
+
+def logout_req(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("homepage")
